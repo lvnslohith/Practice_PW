767f3849d7dc229ce66356c32bbbdd26d98d4159
Lohith Pericherla <lohith891000@gmail.com>
2026-06-09 19:48:10 +0530

    first_commit

--- tests/user-management/login-valid.spec.ts
+++ tests/user-management/login-valid.spec.ts
@@ -15,6 +15,7 @@ test.describe('User Registration and Authentication', () => {
     await page.locator(Locators.LoginButton).click();
     // expect: The login is successful — verify account options are shown (e.g., 'Log out')
     await expect(page.getByRole('link', { name: 'Log out' })).toBeVisible();
+    test
     
   });
 });
